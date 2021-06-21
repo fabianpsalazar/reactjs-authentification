5554990bb1dcf6b2d9948a65564719f38d4ae520
geocodinglife <geocodinglife@gmail.com>
2021-06-21 09:50:27 -0500

    token is working

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
 import './Login.css'
 
-function Login() {
+async function loginUser(credentials) {
+  return fetch('https://hidden-dusk-35513.herokuapp.com/users/sign_in', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+  .then(data => data.json())
+}
+
+function Login({ setToken }) {
+  const [email, setEmail]       = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const token = await loginUser({
+      user: {
+        email,
+        password
+      }
+    })
+    setToken(token)
+  }
+
   return (
     <div className='login-wrapper'>
       <h1>Please Log In</h1>
-    <form>
+    <form onSubmit={ handleSubmit }>
         <label>
-            <p>Username</p>
-            <input type='text'/>
+            <p>Email</p>
+            <input type='text' onChange={ e => setEmail(e.target.value)} />
         </label>
         <label>
             <p>Password</p>
-            <input type='password'/>
+            <input type='password' onChange={e => setPassword(e.target.value)} />
         </label>
         <div>
             <button type='submit'>Submit</button>
@@ -20,6 +46,10 @@ function Login() {
     </form>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
 
 export default Login
